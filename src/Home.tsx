d058192968168c8da80662106479b28e3aaa7bc7
Chris Ho <chris.ho@technet-asia.com>
2022-06-28 10:49:57 +0800

    feat(home): include Navbar and Header component in Homepage

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+
+const Home: FC = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+};
+
+export default Home;
